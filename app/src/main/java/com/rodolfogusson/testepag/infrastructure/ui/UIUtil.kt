ea46c55e06ba1223cce5a9ebb50d30f80f84dffd
Rodolfo <rodolfo@sazso.com.br>
2019-09-19 17:54:01 -0300

    Implemented fake favorite action for button in details screen.

--- app/src/main/java/com/rodolfogusson/testepag/infrastructure/ui/UIUtil.kt
+++ app/src/main/java/com/rodolfogusson/testepag/infrastructure/ui/UIUtil.kt
@@ -1,8 +1,14 @@
 package com.rodolfogusson.testepag.infrastructure.ui
 
+import androidx.appcompat.widget.AppCompatButton
 import com.rodolfogusson.testepag.model.Genre
 import org.threeten.bp.LocalDate
 import org.threeten.bp.format.DateTimeFormatter
+import android.content.Context
+import androidx.core.content.ContextCompat
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
 
 class UIUtil {
     companion object {
@@ -19,5 +25,20 @@ class UIUtil {
 
         @JvmStatic
         fun voteCountString(count: Int) = "$count"
+
+        fun setButtonColor(color: Int, button: AppCompatButton, context: Context) {
+            when (val background = button.background) {
+                is ShapeDrawable -> background.paint.color =
+                    ContextCompat.getColor(context, color)
+                is GradientDrawable -> background.setColor(
+                    ContextCompat.getColor(
+                        context,
+                        color
+                    )
+                )
+                is ColorDrawable -> background.color =
+                    ContextCompat.getColor(context, color)
+            }
+        }
     }
 }
--- app/src/main/java/com/rodolfogusson/testepag/view/moviesdetails/MovieDetailsActivity.kt
+++ app/src/main/java/com/rodolfogusson/testepag/view/moviesdetails/MovieDetailsActivity.kt
@@ -1,14 +1,18 @@
 package com.rodolfogusson.testepag.view.moviesdetails
 
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import androidx.core.graphics.drawable.toDrawable
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.rodolfogusson.testepag.R
 import com.rodolfogusson.testepag.databinding.ActivityMoviesDetailsBinding
+import com.rodolfogusson.testepag.infrastructure.ui.UIUtil
 import com.rodolfogusson.testepag.viewmodel.moviesdetails.MovieDetailsViewModel
 import com.rodolfogusson.testepag.viewmodel.moviesdetails.MovieDetailsViewModelFactory
 import kotlinx.android.synthetic.main.activity_movies_details.*
@@ -31,6 +35,12 @@ class MovieDetailsActivity : AppCompatActivity() {
         observeMovie()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        favoriteButton.setOnClickListener {
+            favoriteButton.background = resources.getDrawable(R.drawable.round_green_button)
+            favoriteButton.setCompoundDrawablesWithIntrinsicBounds(resources.getDrawable(R.drawable.check), null, null, null)
+            favoriteButton.text = "FAVORITO"
+        }
     }
 
     private fun observeMovie() {
